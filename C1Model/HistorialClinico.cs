66892c2a809994f2bc1d473f06c718c49b082708
aftenegusnay <102046581+aftenegusnay@users.noreply.github.com>
2023-08-11 02:22:31 -0500

    Add files via upload
    Adjunto la creacion de las primeras 5 tablas

---
+++ C1Model/HistorialClinico.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TrabajoFinalGrupal.C1Models
+{
+    [Table ("Historial Clinico")]
+    public class HistorialClinico
+    {
+        [Key]
+        public int Id_HisClinico { get; set; }
+        public string Descripcion { get; set; }
+        public string NotasMedicas { get; set; }
+        public string ListEnfermedadesRecient { get; set; }
+
+        [ForeignKey ("Paciente")]
+        public int Id_Paciente { get; set; }
+        public virtual Paciente Paciente { get; set; }
+
+    }
+}
---
+++ C1Model/Persona.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TrabajoFinalGrupal.C1Models
+{
+    [Table ("Persona")]
+    public class Persona
+    {
+        [Key]
+        public int Id_Persona { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Email { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public string Contrasena { get; set; }
+
+        [Required]
+        public virtual ICollection<Paciente> Paciente { get; set; }
+
+        [Required]
+        public virtual ICollection<Profesion> Profesion { get; set; }
+
+        [Required]
+        public virtual ICollection<Administrador> Administrador { get; set; }
+
+    }
+}

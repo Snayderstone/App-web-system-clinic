b4e1c8a0e92a7bc9f5006c0cc14e9e1863314229
damian-alexander-cando <dacandot1@uce.edu.ec>
2023-08-28 08:21:28 -0500

    direccionview

---
+++ C1Model/DireccionCD.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AppWebSistemaClinica.C1Model
+{
+    [Table("DIRECCIONCD")]
+    public class DireccionCD
+    {
+        [Key]
+        public int Id_Direccion { get; set; }
+
+        [ForeignKey("TipoDireccion")]
+        public int Id_TipoDireccion { get; set; }
+
+        [ForeignKey("PACIENTES")]
+        public int idPaciente { get; set; }
+
+        [Required]
+        [MaxLength(250)]
+        public string Calle_Principal { get; set; }
+
+        [MaxLength(200)]
+        public string Calle_Secundaria { get; set; }
+
+        [Required]
+        [MaxLength(10)]
+        public string Num_Casa { get; set; }
+
+        [MaxLength(250)]
+        public string Referencia { get; set; }
+
+        [Required]
+        [MaxLength(1)]
+        public string Estado { get; set; }
+
+        public TipoDireccionCD TipoDireccion { get; set; }
+
+        [Required]
+        public virtual TipoDireccionCD TipoDireccionCD { get; set; }
+    }
+}
---
+++ Models/DirecionViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+public class DirecionViewModel
+{
+    [Required(ErrorMessage = "La cédula es obligatoria.")]
+    public string Cedula { get; set; }
+
+    [Required(ErrorMessage = "El nombre es obligatorio.")]
+    public string Nombres { get; set; }
+
+    [Required(ErrorMessage = "El apellido es obligatorio.")]
+    public string Apellidos { get; set; }
+
+    [Required(ErrorMessage = "La descripción del tipo de dirección es obligatoria.")]
+    public string DescripcionTipoDireccion { get; set; }
+
+    [Required(ErrorMessage = "La calle principal es obligatoria.")]
+    public string CallePrincipal { get; set; }
+
+    [Required(ErrorMessage = "La calle secundaria es obligatoria.")]
+    public string CalleSecundaria { get; set; }
+
+    [Required(ErrorMessage = "El número de casa es obligatorio.")]
+    public string NumCasa { get; set; }
+
+    [Required(ErrorMessage = "El estado de la dirección es obligatorio.")]
+    public string EstadoDireccion { get; set; }
+}
